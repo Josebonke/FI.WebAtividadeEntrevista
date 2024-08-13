a51709f2ff1587a45a938eb7c304e2a43f5ce4e1
unknown <jcbonke@yahoo.com.br>
2024-08-13 13:36:26 -0300

    inital

---
+++ FI.AtividadeEntrevista/DML/Cliente.cs
@@ -0,0 +1,61 @@
+﻿namespace FI.AtividadeEntrevista.DML
+{
+    /// <summary>
+    /// Classe de cliente que representa o registo na tabela Cliente do Banco de Dados
+    /// </summary>
+    public class Cliente
+    {
+        /// <summary>
+        /// Id
+        /// </summary>
+        public long Id { get; set; } = long.MinValue;
+
+        /// <summary>
+        /// CEP
+        /// </summary>
+        /// </summary>
+        public string Cep { get; set; } = string.Empty;
+
+        public string Cpf { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Cidade
+        /// </summary>
+        public string Cidade { get; set; } = string.Empty;
+
+        /// <summary>
+        /// E-mail
+        /// </summary>
+        public string Email { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Estado
+        /// </summary>
+        public string Estado { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Logradouro
+        /// </summary>
+        public string Logradouro { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Nacionalidade
+        /// </summary>
+        public string Nacionalidade { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        public string Nome { get; set; } = string.Empty;
+
+        /// <summary>
+        /// Sobrenome
+        /// </summary>
+        public string Sobrenome { get; set; }
+
+        /// <summary>
+        /// Telefone
+        /// </summary>
+        public string Telefone { get; set; }
+    }
+}
---
+++ FI.WebAtividadeEntrevista/Scripts/Clientes/mask.js
@@ -0,0 +1,33 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+   
+    document.getElementById('cpf').addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, ''); 
+        if (value.length > 11) {
+            value = value.slice(0, 11);
+        }
+        e.target.value = value
+            .replace(/(\d{3})(\d{3})/, '$1.$2.')
+            .replace(/(\d{3})(\d{2})$/, '$1-$2');
+    });
+
+   
+    document.getElementById('CEP').addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, ''); 
+        if (value.length > 8) {
+            value = value.slice(0, 8);
+        }
+        e.target.value = value
+            .replace(/(\d{5})(\d{0,3})/, '$1-$2');
+    });
+
+   
+    document.getElementById('Telefone').addEventListener('input', function (e) {
+        let value = e.target.value.replace(/\D/g, ''); 
+        if (value.length > 11) {
+            value = value.slice(0, 11);
+        }
+        e.target.value = value
+            .replace(/(\d{2})(\d{0,5})/, '($1) $2')
+            .replace(/(\d{5})(\d{0,4})$/, '$1-$2'); 
+    });
+});
